1217200086dc997f4645dea00496b78729b9d438
Paul T. Edlefsen <pedlefsen@gmail.com>
2011-07-08 22:40:17 -0700

    Changed license to Apache v2.0

--- Algebra.hpp
+++ Algebra.hpp
@@ -9,7 +9,7 @@
  *    derived by Ian Holmes and Gerton Lunter from Holmes I, Rubin GM. An
  *    expectation maximization algorithm for training hidden substitution
  *    models.  J Mol Biol. 2002 Apr 12;317(5):753-64.  You may use at will,
- *    subject to the license (LGPL v3), but *please cite those two papers* in
+ *    subject to the license (Apache v2.0), but *please cite those two papers* in
  *    your documentation and publications associated with uses of this library.
  *    Thank you!
  *
@@ -20,18 +20,17 @@
  *    This file is based on algebras.h in HMMoC 1.3, a hidden Markov model
  *    compiler.  Copyright (C) 2007 by Gerton Lunter, Oxford University.
  *
- *    HMMoC-BFloat-Algebra is free software: you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser Public License as published
- *    by the Free Software Foundation, either version 3 of the License, or (at
- *    your option) any later version.
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
  *    
- *    HMMoC-BFloat-Algebra is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU Lesser Public License for more details.
+ *        http://www.apache.org/licenses/LICENSE-2.0
  *    
- *    You should have received a copy of the GNU Lesser Public License along
- *    with HMMoC-BFloat-Algebra.  If not, see <http://www.gnu.org/licenses/>.
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
 \*/
 //
 // Algebra.hpp - extended real types
--- Tests.cpp
+++ Tests.cpp
@@ -1,3 +1,37 @@
+/*
+ *    This file is part of HMMoC-BFloat-Algebra, a tiny C++ library supplying
+ *    the Algebra<> template, for polymorphic representation of non-negative
+ *    floating-point values in either native (eg double), log-transformed, or
+ *    BFloat representations.  BFloat is the Buoyant Float type, allowing
+ *    fixed-precision representation at an arbitrary precision range.  The
+ *    library is based on code from: Lunter G. HMMoC—a compiler for hidden
+ *    Markov models. Bioinformatics (2007) 23(18): 2485-2487. The code was
+ *    derived by Ian Holmes and Gerton Lunter from Holmes I, Rubin GM. An
+ *    expectation maximization algorithm for training hidden substitution
+ *    models.  J Mol Biol. 2002 Apr 12;317(5):753-64.  You may use at will,
+ *    subject to the license (Apache v2.0), but *please cite those two papers* in
+ *    your documentation and publications associated with uses of this library.
+ *    Thank you!
+ *
+ *
+ *    Copyright (C) 2011 by Paul T Edlefsen, Fred Hutchinson Cancer Research
+ *    Center.
+ *
+ *    This file is based on algebras.cc in HMMoC 1.3, a hidden Markov model
+ *    compiler.  Copyright (C) 2007 by Gerton Lunter, Oxford University.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *    
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *    
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+\*/
 #include "Algebra.hpp"
 
 #include <limits>
